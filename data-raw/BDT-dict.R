fce895315d911b9826cb4459a79fd7b3b75ceb05
klaus <kgf@omniversum.de>
2023-04-20 21:00:46 +0200

    Changed raw dict format.

--- data-raw/BDT-dict.R
+++ data-raw/BDT-dict.R
@@ -1,4 +1,5 @@
-BDT_dict_raw <- readRDS("data-raw/BDT_dict.RDS")
+#BDT_dict_raw <- readRDS("data-raw/BDT_dict.RDS")
+BDT_dict_raw <- readxl::read_excel("data-raw/BDT_dict.xlsx")
 BDT_dict_raw[BDT_dict_raw$key == "SAMPLE_QUESTION", "DE"] <- "**Beispiel {{no_example}}/2**\\\\War der Beep-Ton **auf** oder **neben** dem Beat?"
 BDT_dict <- psychTestR::i18n_dict$new(BDT_dict_raw)
 usethis::use_data(BDT_dict, overwrite = TRUE)
